65755df3ce9d74a253467678f076a5818528c691
Rachit Patel <rachit8151@gmail.com>
2026-02-14 14:48:18 +0530

    first commit

---
+++ number-class/number-class/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Number!");
+
+//int x = 21000000000;
+//Console.WriteLine( checked(x + x));
+
+void Calculation()
+{
+	int a = 30;
+	int b = 15;
+
+	long c;
+
+	c = (long)a + (long)b;
+	Console.WriteLine("Sum of " + a + " and " + b + " is: " + c);
+	c = (long)a - (long)b;
+	Console.WriteLine("Sub of " + a + " and " + b + " is: " + c);
+	c = (long)a * (long)b;
+	Console.WriteLine("Mul of " + a + " and " + b + " is: " + c);
+	c = (long)a / (long)b;
+	Console.WriteLine("Div of " + a + " and " + b + " is: " + c);
+
+}
+Calculation();
+
+int w = (int)55.5;
+double x = 22.2; // natural type
+float y = 12.2F;
+decimal z = 21.2M; // explicit type
+Console.WriteLine(" int " + w + " double " + x + " float stuffix F " + y + " decimal stuffix M " + z);
+
+int max = int.MaxValue;
+Console.WriteLine($"{max}");
+
+Console.ReadLine();
+
+
+
---
+++ string-class/string-class/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("         Hello, Patel!");
+
+string firstName = "    Rachit";
+string middleName = "K";
+string lastName = "    Patel    ";
+Console.WriteLine($"Full Name: {firstName.TrimStart()} {middleName} {lastName.Trim()}");
+
+string friendName = "Kenil    ";
+Console.WriteLine("My Friend Name is : " + friendName.TrimEnd()  + ".");
+
+string fullName = $"Full Name: {firstName.TrimStart()} {middleName} {lastName.Trim()}";
+
+fullName = fullName.Replace("K", "Khushalbhai");
+
+//Console.WriteLine(fullName);
+
+Console.WriteLine("upper case ".ToUpper() + friendName.ToUpper());
+Console.WriteLine("LOWER CASE ".ToLower());
+
+Console.WriteLine(fullName.Length);
+
+Console.WriteLine(friendName.StartsWith("K"));
+Console.WriteLine(friendName.EndsWith("l"));
+
+Console.ReadLine();
